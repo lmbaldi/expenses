d5c85ff00926ef7a13f246158c7d410e39413009
lmbaldi <marcelobaldi@celepar.pr.gov.br>
2020-03-17 09:20:13 -0300

    add file that contains transaction list user

---
+++ lib/components/transaction_user.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'transaction_list.dart';
+import 'transaction_form.dart';
+import '../models/transaction.dart';
+
+class TransactionUser extends StatefulWidget {
+  @override
+  _TransactionUserState createState() => _TransactionUserState();
+}
+
+class _TransactionUserState extends State<TransactionUser> {
+
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo tênis de corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de energia',
+      value: 311.30,
+      date: DateTime.now(),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TransactionList(_transactions),
+        TransactionForm(),
+      ],
+    );
+  }
+}
