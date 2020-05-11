25e3bd84c13183edfd4fe2ca8893a77dd3e3d6ff
lmbaldi <luizmarcelobaldi@hotmail.com>
2020-05-11 09:59:42 -0300

    alter transaction_list.dart, chart.dart, chart_bar.dart

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -11,68 +11,53 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 450,
-      child: transactions.isEmpty 
-      ? Column(
-        children: <Widget>[
-          Text("Nenhuma transação cadastrada!",
-          style: Theme.of(context).textTheme.title,),
-          SizedBox(height: 20,),
-          Container(
-            height: 200,
-            child: Image.asset('assets/images/waiting.png',
-              fit: BoxFit.cover,
-              ),
-          ),
-        ],
-      ) 
-      : ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (context, index) {
-          final tr = transactions[index];
-          return Card(
-            child: Row(
+      child: transactions.isEmpty
+          ? Column(
               children: <Widget>[
+                Text(
+                  "Nenhuma transação cadastrada!",
+                  style: Theme.of(context).textTheme.title,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
                 Container(
-                  margin: EdgeInsets.symmetric(
-                    horizontal: 15,
-                    vertical: 10,
-                  ),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Theme.of(context).primaryColor,
-                      width: 2,
-                    ),
-                  ),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    'R\$ ${tr.value.toStringAsFixed(2)}',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                       color: Theme.of(context).primaryColor,
-                    ),
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
                   ),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
+              ],
+            )
+          : ListView.builder(
+              itemCount: transactions.length,
+              itemBuilder: (context, index) {
+                final tr = transactions[index];
+                return Card(
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: FittedBox(
+                          child: Text('R\$ ${tr.value}'),
+                        ),
+                      ),
+                    ),
+                    title: Text(
                       tr.title,
                       style: Theme.of(context).textTheme.title,
                     ),
-                    Text(
+                    subtitle: Text(
                       DateFormat('d MMM y').format(tr.date),
-                      style: TextStyle(
-                        color: Colors.grey[600],
-                      ),
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                );
+              },
             ),
-          );
-        },
-      ),
     );
   }
 }
