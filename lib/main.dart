eb14d15f3070a88f3b08790c0207b1bd5ce9a524
lmbaldi <luizmarcelobaldi@hotmail.com>
2020-05-13 10:19:01 -0300

    remover item da lista

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,10 @@ class ExpensesApp extends StatelessWidget {
               fontSize: 18,
               fontWeight: FontWeight.bold
             ),
+            button:TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold
+            ),
         ),
         appBarTheme: AppBarTheme(
           textTheme: ThemeData.light().textTheme.copyWith(
@@ -47,27 +51,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  final List<Transaction> _transactions = [
-    Transaction(
-      id: 't0',
-      title: 'Conta telefone',
-      value: 54.76,
-      date: DateTime.now().subtract(Duration(days: 30)),
-    ),
-    Transaction(
-      id: 't1',
-      title: 'Novo tênis de corrida',
-      value: 310.76,
-      date: DateTime.now().subtract(Duration(days: 3)),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Conta de energia',
-      value: 311.30,
-      date: DateTime.now().subtract(Duration(days: 4)),
-    ),
-    
-  ];
+  final List<Transaction> _transactions = [];
 
   List<Transaction> get _recentTransactions{
     return _transactions.where((tr){
@@ -77,18 +61,25 @@ class _MyHomePageState extends State<MyHomePage> {
     }).toList();
   }
 
-  _addTransaction(String title, double value){
+  _addTransaction(String title, double value, DateTime date){
     final newTransaction = Transaction(
       id: Random().nextDouble().toString(),
       title: title,
       value: value,
-      date: DateTime.now(),
+      date: date,
     );
     setState(() {
       _transactions.add(newTransaction);
     });
 
     Navigator.of(context).pop();
+  }
+
+  _removeTransaction(String id){
+    setState(() {
+      _transactions.removeWhere((tr) => tr.id == id);
+      
+    });
   }
 
   _openTransactionFormModal(BuildContext context){
@@ -118,7 +109,7 @@ class _MyHomePageState extends State<MyHomePage> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Chart(_recentTransactions),
-            TransactionList(_transactions),
+            TransactionList(_transactions,_removeTransaction),
           ],
         ),
       ),
